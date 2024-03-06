26f0159abbb4829ecc544d8fa187ced54b2a1641
szilin08 <153799833+szilin08@users.noreply.github.com>
2024-03-06 13:01:56 +0000

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <!-- TOP IMAGE -->
 <section class="topSingleBkg topPageBkg">
-  <div class="item-img top-blog-grid-3cols"></div>
+  
    <div class="inner-desc">
       <h1 class="post-title single-post-title">Our Services</h1>
       <span class="post-subtitle">Latest News</span>
